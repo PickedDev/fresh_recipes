3e778683a2e85180965f7aa62d1cae9a3447e004
Anton Imanov <antony.imanov@gmail.com>
2021-07-06 18:40:43 +0300

    Fixed export bug

--- src/App.js
+++ src/App.js
@@ -3,29 +3,30 @@ import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
-import {SearchAppBar} from './components/ui/Header'
+import Header from './components/ui/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-     
-      </header>
-      <Button variant="contained" color="primary" href="#contained-buttons">
-        Link
-      </Button>
+      <Header >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Button variant="contained" color="primary" href="#contained-buttons">
+          Link
+        </Button>
+      </Header >
     </div>
   );
 }
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar';
 
-export default function Header() {
-
+function Header() {
   return (
     <div className='1212'>
       <AppBar position="sticky">
-     
-       
+        1323
+
       </AppBar>
     </div>
   );
 }
+
+
+export default Header;
