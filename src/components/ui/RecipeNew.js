22fe1bb455273a6db4fe0e541a13af426ffe6561
NuclMe <saltansergei@gmail.com>
2021-10-25 21:11:27 +0300

    synchronised-dev-with-main

--- src/components/ui/RecipeNew.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { fade, makeStyles } from '@material-ui/core/styles';
-import RecipeNewForm from './RecipeNewForm';
-
-export default function RecipeNew() {
-    return (
-        <RecipeNewForm />
-    )
-}
