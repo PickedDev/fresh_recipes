bd134ba06469f742c29e4a236eaefefe6565f8cf
Anton Imanov <antony.imanov@gmail.com>
2021-11-18 20:24:33 +0200

    Reverted changes for forms

--- src/components/ui/SignUp/SignUp.tsx
+++ src/components/ui/SignUp/SignUp.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import history from '../../../@history';
-import { useForm, FormProvider } from 'react-hook-form';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import { useStyles } from './styles';
+import passwordClock from '../../img/password.svg';
+import sandClock from '../../img/sand.svg';
+import blue from '../../img/checkblue.svg';
+import grey from '../../img/checkgrey.svg';
 
 export const SignUp = () => {
     const classes = useStyles();
@@ -11,22 +19,11 @@ export const SignUp = () => {
         showPassword: false,
     });
 
-    const methods = useForm();
-
-    /* TO DO
-        1. SignIn / SignUp pages should contain only AuthForm component with different props
-        2. Check if page works
-    */
-
     const handleClickShowPassword = () => {
         setValues({
             ...values,
             showPassword: !values.showPassword,
         });
-    };
-
-    const onSubmit = async (model: { email: string; password: string }) => {
-        // history.push('/');
     };
 
     return (
@@ -40,12 +37,74 @@ export const SignUp = () => {
                             Please enter your accounts here
                         </span>
                     </div>
-                    <FormProvider {...methods}>
-                        <form
-                            className={classes.form}
-                            onSubmit={methods.handleSubmit(onSubmit)}
-                        ></form>
-                    </FormProvider>
+                    <form className={classes.form}>
+                        <div className={classes.inputs}>
+                            <FormControl variant="standard" className={classes.inputWrapper}>
+                                <InputBase
+                                    id="input-with-icon-adornment"
+                                    startAdornment={
+                                        <InputAdornment position="start">
+                                            <img
+                                                src={sandClock}
+                                                alt={sandClock}
+                                                className={classes.InputWrapperImg}
+                                            />
+                                        </InputAdornment>
+                                    }
+                                />
+                            </FormControl>
+                            <FormControl variant="standard" className={classes.inputWrapper}>
+                                <InputBase
+                                    id="outlined-adornment-password"
+                                    type={values.showPassword ? 'text' : 'password'}
+                                    startAdornment={
+                                        <InputAdornment position="start">
+                                            <img
+                                                src={passwordClock}
+                                                alt={passwordClock}
+                                                className={classes.InputWrapperImg}
+                                            />
+                                        </InputAdornment>
+                                    }
+                                    endAdornment={
+                                        <>
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    className={classes.iconEye}
+                                                    aria-label="toggle password visibility"
+                                                    edge="end"
+                                                    onClick={handleClickShowPassword}
+                                                >
+                                                    {values.showPassword ? (
+                                                        <VisibilityOff />
+                                                    ) : (
+                                                        <Visibility />
+                                                    )}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        </>
+                                    }
+                                />
+                            </FormControl>
+                        </div>
+
+                        <div className={classes.requirmentsPassword}>
+                            Your password must contain:
+                        </div>
+                        <div className={classes.requimentsWrapper}>
+                            <div className={classes.requirments}>
+                                <img src={blue} alt={blue} className={classes.requirmentsImg} />
+                                Atleast 6 characters
+                            </div>
+                            <div className={classes.requirments}>
+                                <img src={grey} alt={grey} className={classes.requirmentsImg} />
+                                Contain a number
+                            </div>
+                        </div>
+                        <button type="submit" className={classes.formBtn}>
+                            Sign Up
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
