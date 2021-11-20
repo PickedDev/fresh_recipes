83d354ca6ac33c88d032b3c5fcc3347fa148eb29
NuclMe <saltansergei@gmail.com>
2021-11-21 00:39:18 +0200

    added-styles-on-form

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Dialog } from '@material-ui/core';
-import InputLabel from '@material-ui/core/InputLabel';
+import Button from '@mui/material/Button';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
-import Input from '@material-ui/core/Input';
-import FilledInput from '@material-ui/core/FilledInput';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
 import InputAdornment from '@material-ui/core/InputAdornment';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import Link from '@material-ui/core/Link';
+import InputBase from '@material-ui/core/InputBase';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
 import sandClock from '../../assets/img/sand.svg';
 import passwordClock from '../../assets/img/password.svg';
 import { useStyles } from './styles';
 
 export const SignIn = () => {
+    const [open, setOpen] = React.useState(false);
     const classes = useStyles();
     const [values, setValues] = React.useState({
         amount: '',
@@ -27,6 +27,14 @@ export const SignIn = () => {
         weightRange: '',
         showPassword: false,
     });
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const handleChange = (prop: any) => (event: any) => {
         setValues({ ...values, [prop]: event.target.value });
@@ -41,94 +49,98 @@ export const SignIn = () => {
     };
 
     return (
-        <div>
-            <p> Sign In Form</p>
-            <div>
-                <p> Welcome back! </p>
-                <span>Please enter your accounts here </span>
-            </div>
-            <Formik // i haven't installed formik so won't work
-                initialValues={{ email: '', password: '' }}
-                validate={(values: any) => {
-                    const errors = { email: '', password: '' };
+        <div className={classes.container}>
+            <div className={classes.formWrapper}>
+                <h1 className={classes.title}> Sign In Form</h1>
+                <div className={classes.formInner}>
+                    <div className={classes.subtitle}>
+                        <p className={classes.subtitleMessauge}> Welcome! </p>
+                        <span className={classes.subtitleText}>
+                            Please enter your accounts here
+                        </span>
+                    </div>
+                    <form className={classes.form}>
+                        <div className={classes.inputs}>
+                            <FormControl variant="standard" className={classes.inputWrapper}>
+                                <InputBase
+                                    id="input-with-icon-adornment"
+                                    startAdornment={
+                                        <InputAdornment position="start">
+                                            <img
+                                                src={sandClock}
+                                                alt={sandClock}
+                                                className={classes.InputWrapperImg}
+                                            />
+                                        </InputAdornment>
+                                    }
+                                />
+                            </FormControl>
+                            <FormControl variant="standard" className={classes.inputWrapper}>
+                                <InputBase
+                                    id="outlined-adornment-password"
+                                    type={values.showPassword ? 'text' : 'password'}
+                                    startAdornment={
+                                        <InputAdornment position="start">
+                                            <img
+                                                src={passwordClock}
+                                                alt={passwordClock}
+                                                className={classes.InputWrapperImg}
+                                            />
+                                        </InputAdornment>
+                                    }
+                                    endAdornment={
+                                        <>
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    className={classes.iconEye}
+                                                    aria-label="toggle password visibility"
+                                                    edge="end"
+                                                    onClick={handleClickShowPassword}
+                                                >
+                                                    {values.showPassword ? (
+                                                        <VisibilityOff />
+                                                    ) : (
+                                                        <Visibility />
+                                                    )}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        </>
+                                    }
+                                />
+                            </FormControl>
+                        </div>
 
-                    if (!values.email) {
-                        errors.email = 'Required';
-                    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-                        errors.email = 'Invalid email address';
-                    }
-
-                    return errors;
-                }}
-                onSubmit={(values: any, { setSubmitting: any }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        setSubmitting(false);
-                    }, 400);
-                }}
-            >
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting,
-                    /* and other goodies */
-                }) => (
-                    <form onSubmit={handleSubmit}>
-                        <InputLabel
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            // value={values}
-                        />
-                        <img src={sandClock} alt={sandClock} />
-
-                        <Input
-                            type="email"
-                            name="email"
-                            // onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.email}
-                            id="standard-adornment-password"
-                            // type={values.email ? 'text' : 'email'}
-                            // value={values.password}
-                            onChange={handleChange('email')}
-                        />
-                        {errors.email && touched.email && errors.email}
-                        <InputLabel htmlFor="standard-adornment-password" />
-                        <img src={passwordClock} alt={sandClock} />
-
-                        <Input
-                            // type="password"
-                            name="password"
-                            // onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.password}
-                            id="standard-adornment-password"
-                            type={values.showPassword ? 'text' : 'password'}
-                            onChange={handleChange('password')}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                    >
-                                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                        />
-                        {errors.password && touched.password && errors.password}
-                        <div>Forgot password?</div>
-                        <button type="submit" disabled={isSubmitting}>
+                        <div className={classes.reset}>
+                            <a href="#/" className={classes.resetText} onClick={handleClickOpen}>
+                                Forgot password?
+                            </a>
+                        </div>
+                        <Dialog
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="alert-dialog-title"
+                            aria-describedby="alert-dialog-description"
+                            maxWidth="lg"
+                        >
+                            <DialogTitle id="alert-dialog-title">Reset password</DialogTitle>
+                            <DialogContent>
+                                <TextField
+                                    id="outlined-basic"
+                                    label="Outlined"
+                                    variant="standard"
+                                />
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleClose}>Cancel</Button>
+                                <Button onClick={handleClose}>Reset</Button>
+                            </DialogActions>
+                        </Dialog>
+                        <button type="submit" className={classes.formBtn}>
                             Login
                         </button>
                     </form>
-                )}
-            </Formik>
+                </div>
+            </div>
         </div>
     );
 };
